6fd11f372a995005f5a074b2e0276c381c59a6cc
Wildan Belfiore <mikewildan@outlook.com>
2025-04-16 13:56:52 +0700

    penambahan api landing page

---
+++ app/Http/Controllers/Web/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Gallery;
+use App\Models\Mobil;
+use App\Models\Testimoni;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        $gallery = Gallery::latest()->take(6)->get();
+        $mobil = Mobil::latest()->take(6)->get();
+        $testimoni = Testimoni::latest()->take(6)->get();
+
+        return response()->json([
+            'gallery' => $gallery,
+            'mobil' => $mobil,
+            'testimoni' => $testimoni
+        ]);
+    }
+}
---
+++ app/Http/Controllers/Web/WebBlogController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+
+class WebBlogController extends Controller
+{
+    public function index()
+    {
+        $blogs = Blog::where('status', 'publish')
+                     ->latest()
+                     ->get();
+
+        return response()->json($blogs);
+    }
+
+    public function show($slug)
+    {
+        $blog = Blog::where('slug', $slug)
+                    ->where('status', 'publish')
+                    ->firstOrFail();
+
+        return response()->json($blog);
+    }
+}
