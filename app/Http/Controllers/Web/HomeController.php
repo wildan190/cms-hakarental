30e6004977b586b04e9a4edc304b48fc5eb9b99b
Wildan Belfiore <mikewildan@outlook.com>
2025-04-16 21:39:17 +0700

    perbaikan homecontroller

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends Controller
     public function index()
     {
         $gallery = Gallery::latest()->take(6)->get();
-        $mobil = Mobil::latest()->take(6)->get();
+        $mobil = Mobil::whereIn('name', ['avanza', 'sigra', 'reborn', 'zenix'])->latest()->take(4)->get();
         $testimoni = Testimoni::latest()->take(6)->get();
 
         return response()->json([
