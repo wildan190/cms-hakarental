7b693e99a3eb97354600d897625d28c2a1ee1cab
Wildan Belfiore <mikewildan@outlook.com>
2025-04-18 00:58:09 +0700

    search faq

--- app/Http/Controllers/Web/WebFaqController.php
+++ app/Http/Controllers/Web/WebFaqController.php
@@ -4,12 +4,24 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Faq;
+use Illuminate\Http\Request;
 
 class WebFaqController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $faqs = Faq::latest()->get();
+        $query = Faq::query();
+
+        // Jika ada parameter search
+        if ($request->has('search') && $request->search != '') {
+            $keyword = $request->search;
+            $query->where(function ($q) use ($keyword) {
+                $q->where('title', 'LIKE', "%{$keyword}%")
+                  ->orWhere('description', 'LIKE', "%{$keyword}%");
+            });
+        }
+
+        $faqs = $query->latest()->get();
 
         return response()->json([
             'message' => 'Data FAQ berhasil diambil',
