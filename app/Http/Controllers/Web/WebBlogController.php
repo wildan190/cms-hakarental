d5ba254394cc875ee646119778ddaa3d62dd8b08
Wildan Belfiore <mikewildan@outlook.com>
2025-04-19 20:18:55 +0700

    perbaikan paginate blog pada backend

--- app/Http/Controllers/Web/WebBlogController.php
+++ app/Http/Controllers/Web/WebBlogController.php
@@ -5,15 +5,20 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Request;
 
 class WebBlogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $blogs = Cache::remember('web_blogs', now()->addMinutes(10), function () {
+        $page = $request->get('page', 1);
+        $perPage = 6;
+        $cacheKey = "web_blogs_page_{$page}";
+
+        $blogs = Cache::remember($cacheKey, now()->addMinutes(10), function () use ($perPage) {
             return Blog::where('status', 'publish')
                 ->latest()
-                ->get();
+                ->paginate($perPage);
         });
 
         return response()->json($blogs);
