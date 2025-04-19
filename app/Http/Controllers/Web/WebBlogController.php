0b02b9927fa83802ca0da8bbc69454ad2ba1b454
Wildan Belfiore <mikewildan@outlook.com>
2025-04-20 01:27:01 +0700

    tes

--- app/Http/Controllers/Web/WebBlogController.php
+++ app/Http/Controllers/Web/WebBlogController.php
@@ -4,24 +4,41 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Http\Request;
 
 class WebBlogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $blogs = Blog::where('status', 'publish')
-            ->latest()
-            ->get();
+        $cacheKey = 'web_blog_all';
 
-        return response()->json($blogs);
+        $blogs = Cache::store('redis')->remember($cacheKey, now()->addMinutes(10), function () {
+            return Blog::where('status', 'publish')
+                ->latest()
+                ->get();
+        });
+
+        // Simpan cookie halaman yang sedang dilihat (hanya info, tidak memengaruhi data)
+        $cookie = Cookie::make('last_blog_list_viewed', now()->toDateTimeString(), 60);
+
+        return response()->json($blogs)->withCookie($cookie);
     }
 
     public function show($slug)
     {
-        $blog = Blog::where('slug', $slug)
-            ->where('status', 'publish')
-            ->firstOrFail();
+        $cacheKey = "web_blog_slug_{$slug}";
 
-        return response()->json($blog);
+        $blog = Cache::store('redis')->remember($cacheKey, now()->addMinutes(10), function () use ($slug) {
+            return Blog::where('slug', $slug)
+                ->where('status', 'publish')
+                ->firstOrFail();
+        });
+
+        // Simpan cookie slug artikel terakhir yang dibuka
+        $cookie = Cookie::make('last_blog_opened', $slug, 60);
+
+        return response()->json($blog)->withCookie($cookie);
     }
 }
