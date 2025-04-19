e255c89d361bb8923a6dc9dbd2cfd03fa51fd975
Wildan Belfiore <mikewildan@outlook.com>
2025-04-19 20:15:35 +0700

    rollback web blog controller

--- app/Http/Controllers/Web/WebBlogController.php
+++ app/Http/Controllers/Web/WebBlogController.php
@@ -5,19 +5,15 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Http\Request;
 
 class WebBlogController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $page = $request->get('page', 1);
-        $cacheKey = 'web_blogs_page_' . $page;
-
-        $blogs = Cache::remember($cacheKey, now()->addMinutes(10), function () {
+        $blogs = Cache::remember('web_blogs', now()->addMinutes(10), function () {
             return Blog::where('status', 'publish')
                 ->latest()
-                ->paginate(6);
+                ->get();
         });
 
         return response()->json($blogs);
