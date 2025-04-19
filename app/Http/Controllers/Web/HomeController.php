88eccf8303c585a05a0027da63aa32a5ef70043a
Wildan Belfiore <mikewildan@outlook.com>
2025-04-20 00:33:17 +0700

    perbaikan

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -13,9 +13,15 @@ class HomeController extends Controller
     public function index()
     {
         $gallery = Gallery::latest()->take(6)->get();
-        $mobil = Mobil::latest()->take(4)->get();
-        $testimoni = Testimoni::latest()->take(6)->get();
 
+        $mobil = Mobil::where(function ($query) {
+            $query->where('name', 'like', '%Avanza%')
+                ->orWhere('name', 'like', '%Sigra%')
+                ->orWhere('name', 'like', '%Reborn%')
+                ->orWhere('name', 'like', '%Zenix%');
+        })->latest()->take(4)->get();
+
+        $testimoni = Testimoni::latest()->take(6)->get();
         $faq = Faq::latest()->take(5)->get();
 
         return response()->json([
