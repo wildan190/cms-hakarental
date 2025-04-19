a30d85e1905df73e7be3338d99eaffe7bce28b21
Wildan Belfiore <mikewildan@outlook.com>
2025-04-19 20:13:39 +0700

    paginate untuk blog controller

--- app/Http/Controllers/Web/WebBlogController.php
+++ app/Http/Controllers/Web/WebBlogController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\Web;
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Http\Request;
 
 class WebBlogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $blogs = Cache::remember('web_blogs', now()->addMinutes(10), function () {
+        $page = $request->get('page', 1);
+        $cacheKey = 'web_blogs_page_' . $page;
+
+        $blogs = Cache::remember($cacheKey, now()->addMinutes(10), function () {
             return Blog::where('status', 'publish')
                 ->latest()
-                ->get();
+                ->paginate(6);
         });
 
         return response()->json($blogs);
