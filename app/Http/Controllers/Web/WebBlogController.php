323fea023d8bbfc37cc396ff95e888a588518fd0
Wildan Belfiore <mikewildan@outlook.com>
2025-04-19 19:09:18 +0700

    cache view blog

--- app/Http/Controllers/Web/WebBlogController.php
+++ app/Http/Controllers/Web/WebBlogController.php
@@ -4,23 +4,30 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
+use Illuminate\Support\Facades\Cache;
 
 class WebBlogController extends Controller
 {
     public function index()
     {
-        $blogs = Blog::where('status', 'publish')
-            ->latest()
-            ->get();
+        $blogs = Cache::remember('web_blogs', now()->addMinutes(10), function () {
+            return Blog::where('status', 'publish')
+                ->latest()
+                ->get();
+        });
 
         return response()->json($blogs);
     }
 
     public function show($slug)
     {
-        $blog = Blog::where('slug', $slug)
-            ->where('status', 'publish')
-            ->firstOrFail();
+        $cacheKey = 'web_blog_' . $slug;
+
+        $blog = Cache::remember($cacheKey, now()->addMinutes(10), function () use ($slug) {
+            return Blog::where('slug', $slug)
+                ->where('status', 'publish')
+                ->firstOrFail();
+        });
 
         return response()->json($blog);
     }
