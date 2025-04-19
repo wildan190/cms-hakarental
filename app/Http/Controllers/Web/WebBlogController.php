ac17a0c82dace92de645c36cf7b37391edc67894
Wildan Belfiore <mikewildan@outlook.com>
2025-04-20 01:22:17 +0700

    modified:   app/Http/Controllers/Web/WebBlogController.php

--- app/Http/Controllers/Web/WebBlogController.php
+++ app/Http/Controllers/Web/WebBlogController.php
@@ -4,23 +4,32 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
+use Illuminate\Support\Facades\Cache;
 
 class WebBlogController extends Controller
 {
     public function index()
     {
-        $blogs = Blog::where('status', 'publish')
-            ->latest()
-            ->get();
+        $cacheKey = 'web_blog_all';
+
+        $blogs = Cache::store('redis')->remember($cacheKey, now()->addMinutes(10), function () {
+            return Blog::where('status', 'publish')
+                ->latest()
+                ->get();
+        });
 
         return response()->json($blogs);
     }
 
     public function show($slug)
     {
-        $blog = Blog::where('slug', $slug)
-            ->where('status', 'publish')
-            ->firstOrFail();
+        $cacheKey = "web_blog_slug_{$slug}";
+
+        $blog = Cache::store('redis')->remember($cacheKey, now()->addMinutes(10), function () use ($slug) {
+            return Blog::where('slug', $slug)
+                ->where('status', 'publish')
+                ->firstOrFail();
+        });
 
         return response()->json($blog);
     }
