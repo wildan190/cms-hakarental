560442955ef7823e0b275f454639f8f487420671
Wildan Belfiore <mikewildan@outlook.com>
2025-04-20 00:44:57 +0700

    perbaikan

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -7,28 +7,33 @@ use App\Models\Faq;
 use App\Models\Gallery;
 use App\Models\Mobil;
 use App\Models\Testimoni;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        $gallery = Gallery::latest()->take(6)->get();
+        $cachedData = Cache::remember('web_home_data', now()->addMinutes(10), function () {
+            $gallery = Gallery::latest()->take(6)->get();
 
-        $mobil = Mobil::where(function ($query) {
-            $query->where('name', 'like', '%Avanza%')
-                ->orWhere('name', 'like', '%Sigra%')
-                ->orWhere('name', 'like', '%Reborn%')
-                ->orWhere('name', 'like', '%Zenix%');
-        })->latest()->take(4)->get();
+            $mobil = Mobil::where(function ($query) {
+                $query->where('name', 'like', '%Avanza%')
+                      ->orWhere('name', 'like', '%Sigra%')
+                      ->orWhere('name', 'like', '%Reborn%')
+                      ->orWhere('name', 'like', '%Zenix%');
+            })->latest()->take(4)->get();
 
-        $testimoni = Testimoni::latest()->take(6)->get();
-        $faq = Faq::latest()->take(5)->get();
+            $testimoni = Testimoni::latest()->take(6)->get();
+            $faq = Faq::latest()->take(5)->get();
 
-        return response()->json([
-            'gallery' => $gallery,
-            'mobil' => $mobil,
-            'testimoni' => $testimoni,
-            'faq' => $faq,
-        ]);
+            return [
+                'gallery' => $gallery,
+                'mobil' => $mobil,
+                'testimoni' => $testimoni,
+                'faq' => $faq,
+            ];
+        });
+
+        return response()->json($cachedData);
     }
 }
